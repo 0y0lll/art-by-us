7a52319b13f3d308eea0fa1e10c5c9801798ab65
0y0lll <bemyangel2@gmail.com>
2020-07-05 04:04:46 +0900

    -

--- js/include_html.js
+++ js/include_html.js
@@ -2,5 +2,5 @@ $(document).ready(function () {
     // console.log('2')
 
     $('#header').load('../include/header_sub.html')
-    $('#footer').load('../include/footer.html')
+    $('#footer').load('../include/footer_sub.html')
 })
---
+++ js/include_html_host.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    // console.log('2')
+
+    $('#header').load('../include/header_host_sub.html')
+    $('#footer').load('../include/footer_host_sub.html')
+})
